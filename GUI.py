1a81bba42936f9a808a0a9d97288f82b7645b4c4
ChukwuemekaAjaero <emeka.ajaero7@gmail.com>
2020-01-13 12:51:13 -0500

    Made some more changes to the GUI

--- GUI.py
+++ GUI.py
@@ -21,7 +21,7 @@ class Button:
         pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.height))
 
         if self.text != '':
-            font = pygame.font.SysFont('comicsans', 60)
+            font = pygame.font.SysFont('comicsans', 20)
             text = font.render(self.text, 1, (0,0,0))
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
@@ -32,5 +32,44 @@ class Button:
         if position[0] > self.x and position[0] < self.x + self.width:
             if position[1] > self.y and position[1] < self.y + self.height:
                 return True
-
         return False
+
+class Square:
+
+    isClicked = False
+    outline = False
+    def __init__(self, colour, x, y, height, text = ''):
+        self.colour = colour
+        self.x = x
+        self.y = y
+        self.height = height
+        self.text = text
+
+    def isOver(self, position):
+        '''Button, tuple -> Boolean
+        Returns True if the mouse position is above the button
+        and False otherwise.'''
+        if position[0] > self.x and position[0] < self.x + self.width:
+            if position[1] > self.y and position[1] < self.y + self.height:
+                return True
+        return False
+
+def formatTime(secs):
+    sec = secs%60
+    minute = secs//60
+    hour = minute//60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
+
+def redrawWindow(win, board, playTime):
+    win.fill((255, 255, 255))
+    pygame.draw.line(win, (0, 0, 0), (150, 0), (150, 450), 2)
+    pygame.draw.line(win, (0, 0, 0), (300, 0), (300, 450), 2)
+    pygame.draw.line(win, (0, 0, 0), (0, 150), (450, 150), 2)
+    pygame.draw.line(win, (0, 0, 0), (0, 300), (450, 300), 2)
+    pygame.draw.line(win, (0, 0, 0), (0, 450), (450, 450), 2)
+    #Drawing the timer:
+    fnt = pygame.font.SysFont('comicsans', 30)
+    text = fnt.render("Time: " + formatTime(playTime), 1, (0, 0, 0))
+    win.blit(text, (333, 462))
--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 from SudokuLibrary import *
 import pygame
-import GUI
+from GUI import *
+import time
 run = True
 win = pygame.display.set_mode((450, 500))
+win.fill((255, 255, 255))
 pygame.display.set_caption("Sudoku")
+x = Board()
+x.generateBoard()
 
+#Test Lines
 x = 50
 y = 50
 width = 50
 height = 50
 velocity = 5
 dumBoard = [[1,2,3],[4,5,6],[7,8,9]]
+solveButton = Button((255, 255, 255), 150, 225, 70, 50, 'Solve')
+#End Test Lines
+
+#Static Elements
+startTime = time.time()
+clearButton = Button((255, 255, 255), 18, 462, 55, 27, 'Clear')
+solveButton = Button((255, 255, 255), 123, 462, 55, 27, 'Solve')
+submitButton = Button((255, 255, 255), 228, 462, 55, 27, 'Submit')
 while run:
-    pygame.time.delay(100)
-
-
-
-    win.fill((255, 255, 255))
-    pygame.draw.rect(win, (255, 255, 255), (0, 0, 500, 00)) #Makes a the background white
-    pygame.draw.line(win, (0, 0, 0), (250, 0), (250, 500), 2)
+    #pygame.time.delay(100)
     pygame.font.init()
-    fnt = pygame.font.SysFont('comicsans', 40)
+    fnt = pygame.font.SysFont('serif', 20)
+    playTime = round(time.time() - startTime)
+    clearButton.draw(win, (0, 0, 0))
+    solveButton.draw(win, (0, 0, 0))
+    submitButton.draw(win, (0, 0, 0))
+    pygame.draw.rect(win, (255, 255, 255), (0, 0, 500, 00))  # Makes a the background white
+
     text = fnt.render(str(dumBoard[1][2]), 1, (0, 0, 0))
     win.blit(text, (30, 30))
     text2 = fnt.render(str(dumBoard[2][2]), 1, (0, 0, 0))
     win.blit(text2, (60, 60))
 
+
     for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_0:
                 dumBoard[2][2] = dumBoard[2][2] + 1
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if solveButton.isOver(pos):
+                solveButton.colour = (34, 24, 84)
+        if event.type == pygame.MOUSEMOTION:
+            if solveButton.isOver(pos):
+                dumBoard[2][2] = dumBoard[2][2] + 00
+    redrawWindow(win, x.board, playTime)
     pygame.display.update()
 
 
